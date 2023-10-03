97170dc17ab107eb4a249bb53aae8539aaa3fd5f
OMARTK1 <elyazidi.omar@gmail.com>
2023-10-02 19:44:25 -0700

    update task8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
+text = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:67] + str[107:112] + str[0:6])
+print(text[39:67] + text[107:112] + text[0:6])
 
