5387092142e98991f941ef39068a201228fee60f
OMARTK1 <elyazidi.omar@gmail.com>
2023-10-25 00:04:05 +0100

    task 10

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -7,13 +7,24 @@
  */
 void print_python_bytes(PyObject *p)
 {
+	PyBytesObject *bytes = (PyBytesObject *)p;
+
 	printf("[.] bytes object info\n");
-	printf("  size: %ld\n", (size_t)PyBytes_Size(p));
-	printf("  trying string: %s\n", PyBytes_AsString(p));
-	printf("  first %ld bytes: ", (size_t)PyBytes_Size(p) < 10 ? (size_t)PyBytes_Size(p) : 10);
-	for (size_t i = 0; i < (size_t)PyBytes_Size(p) && i < 10; ++i)
+
+	if (!PyBytes_Check(p))
 	{
-		printf("%02x ", (unsigned char)PyBytes_AsString(p)[i]);
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	Py_ssize_t size = PyBytes_Size(p);
+
+	printf("  size: %ld\n", size);
+	printf("  first %ld bytes: ", size < 10 ? size : 10);
+
+	for (Py_ssize_t i = 0; i < size && i < 10; ++i)
+	{
+		printf("%02x ", (unsigned char)bytes->ob_sval[i]);
 	}
 	printf("\n");
 }
@@ -24,9 +35,19 @@ void print_python_bytes(PyObject *p)
  */
 void print_python_list(PyObject *p)
 {
+	PyListObject *list = (PyListObject *)p;
+
 	printf("[*] Python list info\n");
+
+	if (!PyList_Check(p))
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+
 	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
-	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+	printf("[*] Allocated = %ld\n", list->allocated);
+
 	for (Py_ssize_t i = 0; i < PyList_Size(p); ++i)
 	{
 		printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
@@ -40,5 +61,12 @@ void print_python_list(PyObject *p)
 void print_python_float(PyObject *p)
 {
 	printf("[.] float object info\n");
+
+	if (!PyFloat_Check(p))
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+		return;
+	}
+
 	printf("  value: %f\n", PyFloat_AsDouble(p));
 }
