727e5874107207972d6bb6a91374b41178fcfcb7
OMARTK1 <elyazidi.omar@gmail.com>
2023-10-25 01:37:38 +0100

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,52 +1,59 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+"""Define a class Square."""
 
 
 class Square:
-    """Represents a square"""
+    """Represent a square."""
+
     def __init__(self, size=0, position=(0, 0)):
-        """Initializes the square with a given size and position"""
-        self.__size = size
-        self.__position = position
+        """Initialize a new square.
+
+        Args:
+            sqr_size (int): The size of the new square.
+            sqr_position (int, int): The position of the new square.
+        """
+        self.sqr_size = size
+        self.sqr_position = position
 
     @property
-    def size(self):
-        """Retrieves the size of the square"""
+    def sqr_size(self):
+        """Get/set the current size of the square."""
         return self.__size
 
-    @size.setter
-    def size(self, value):
-        """Sets the size of the square"""
+    @sqr_size.setter
+    def sqr_size(self, value):
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("sqr_size must be an integer")
         elif value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("sqr_size must be >= 0")
         self.__size = value
 
     @property
-    def position(self):
-        """Retrieves the position of the square"""
+    def sqr_position(self):
+        """Get/set the current position of the square."""
         return self.__position
 
-    @position.setter
-    def position(self, value):
-        """Sets the position of the square"""
-        if not isinstance(value, tuple) or len(value) != 2 or \
-           not all(isinstance(i, int) for i in value) or \
-           not all(i >= 0 for i in value):
-            raise TypeError("position must be a tuple of 2 positive integers")
+    @sqr_position.setter
+    def sqr_position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("sqr_position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        """Computes the area of the square"""
-        return self.__size ** 2
+        """Return the current area of the square."""
+        return self.__size * self.__size
 
     def my_print(self):
-        """Prints the square with '#' characters at the specified position"""
+        """Print the square with the # character."""
         if self.__size == 0:
-            print()
-        else:
-            for _ in range(self.__position[1]):
-                print()
-            for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+            print("")
+            return
+
+        [print("") for _ in range(0, self.__position[1])]
+        for _ in range(0, self.__size):
+            [print(" ", end="") for _ in range(0, self.__position[0])]
+            [print("#", end="") for _ in range(0, self.__size)]
+            print("")
