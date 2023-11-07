6cd74c73707209e85c7b8b03ab471248c0e8d2c9
OMARTK1 <elyazidi.omar@gmail.com>
2023-11-07 03:13:34 +0100

    rest

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module containing the MyInt class.
+"""
+
+class MyInt(int):
+    """
+    MyInt class that inherits from int.
+    """
+    def __eq__(self, other):
+        """
+        Overrides the equality operator.
+
+        Args:
+            other (int): The other integer to compare.
+
+        Returns:
+            bool: True if self is not equal to other, False otherwise.
+        """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """
+        Overrides the inequality operator.
+
+        Args:
+            other (int): The other integer to compare.
+
+        Returns:
+            bool: True if self is equal to other, False otherwise.
+        """
+        return super().__eq__(other)
---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module containing the add_attribute function.
+"""
+
+def add_attribute(obj, name, value):
+    """
+    Adds a new attribute to an object if it's possible.
+
+    Args:
+        obj (object): The object to add the attribute to.
+        name (str): The name of the attribute.
+        value (any): The value of the attribute.
+
+    Raises:
+        TypeError: If the object can't have a new attribute.
+    """
+    if hasattr(obj, "__dict__") or hasattr(obj, "__slots__"):
+        setattr(obj, name, value)
+    else:
+        raise TypeError("can't add new attribute")
+
