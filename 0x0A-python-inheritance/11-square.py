6c69f7a4b1b8efa2508b08e9fa35eda62454e094
OMARTK1 <elyazidi.omar@gmail.com>
2023-11-07 03:03:22 +0100

    update 10 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,45 +1,29 @@
 #!/usr/bin/python3
+"""
+Module containing the Square class inheriting from Rectangle.
+"""
 
-class BaseGeometry:
-    """Empty class BaseGeometry"""
-    
-    def area(self):
-        """Raises an Exception with the message area() is not implemented"""
-        raise Exception("area() is not implemented")
-    
-    def integer_validator(self, name, value):
-        """Validates value as an integer"""
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-class Rectangle(BaseGeometry):
-    """Class Rectangle that inherits from BaseGeometry"""
-    
-    def __init__(self, width, height):
-        """Initializes a Rectangle instance"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Computes the area of the rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Returns the rectangle description [Rectangle] <width>/<height>"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
-    """Class Square that inherits from Rectangle"""
-    
+    """
+    Square class inheriting from Rectangle.
+    """
     def __init__(self, size):
-        """Initializes a Square instance"""
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): Size of the square.
+        """
         self.integer_validator("size", size)
         super().__init__(size, size)
 
     def __str__(self):
-        """Returns the square description [Square] <width>/<height>"""
-        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
+        """
+        Returns the square description.
+
+        Returns:
+            str: Square description.
+        """
+        return "[Square] {}/{}".format(self.__width, self.__height)
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,6 +5,7 @@ Module containing a class Rectangle that inherits from BaseGeometry.
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """
     Class Rectangle that inherits from BaseGeometry.
