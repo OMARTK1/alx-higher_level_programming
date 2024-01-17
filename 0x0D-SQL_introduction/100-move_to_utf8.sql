5bfc0bbee2c18fe38755432204bc5b125e81f9c9
OMARTK1 <elyazidi.omar@gmail.com>
2024-01-17 03:52:26 +0100

    done 17-100  output resolved

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,4 @@
-USE hbtn_0c_0;
-
--- Convert the database character set and collation
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the table character set and collation
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the column character set and collation
-ALTER TABLE first_table MODIFY COLUMN name varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+-- Converts the entire database hbtn_0c_0 to UTF8.
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
