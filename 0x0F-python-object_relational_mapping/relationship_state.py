f7dc4d356df222646e3204fc2a9055184ee0248b
OMARTK1 <elyazidi.omar@gmail.com>
2024-02-19 11:41:29 +0100

    fix 101 state

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+"""Defines the State class."""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -8,6 +10,8 @@ Base = declarative_base()
 
 
 class State(Base):
+    """Represents a state."""
+
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False)
