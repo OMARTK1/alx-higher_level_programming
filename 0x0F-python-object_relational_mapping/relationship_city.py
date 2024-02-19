d46c2cf4244e35c648a0fdafbb018ac811469517
OMARTK1 <elyazidi.omar@gmail.com>
2024-02-19 11:40:02 +0100

    fix 101 city

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
+"""Defines the City class."""
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from relationship_state import Base
 
 
 class City(Base):
+    """Represents a city."""
+
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, nullable=False)
