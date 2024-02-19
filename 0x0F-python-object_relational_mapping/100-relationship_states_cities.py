2834e25c3f842d72bb3ad9d6a568df90e7fef449
OMARTK1 <elyazidi.omar@gmail.com>
2024-02-19 12:32:11 +0100

    fix 100

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-
 """
-This script creates the State "California" with the City "San Francisco"
-in the database.
+This script creates the State "California" with the City "San Francisco" from a DB
 """
-
 import sys
-from sqlalchemy import create_engine
 from relationship_state import Base, State
 from relationship_city import City
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
 
-    engine = create_engine
-    (f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_state = State(name="California", cities=[City(name="San Francisco")])
-    session.add(new_state)
+    newState = State(name='California')
+    newCity = City(name='San Francisco')
+    newState.cities.append(newCity)
+
+    session.add(newState)
+    session.add(newCity)
     session.commit()
--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-
 """
-This module defines the City class, which represents a city entity
-in the database.
+Contains the class definition of a City
 """
-
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 from relationship_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class City(Base):
-    """Represents a city."""
-
+    """
+    Class that defines each city
+    """
     __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
